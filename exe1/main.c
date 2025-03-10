1310298fafdc79f8c294678a59c4968895be3651
iara.vsp <iaravsp@al.insper.edu.br>
2025-03-10 09:05:51 -0300

    exe1

--- exe1/main.c
+++ exe1/main.c
@@ -6,15 +6,27 @@ const int BTN_PIN_R = 28;
 const int LED_PIN_R = 4;
 
 volatile int flag_f_r = 0;
+volatile int timer_estado = 0;
 
-void btn_callback(uint gpio, uint32_t events) {
-    if (events == 0x4) { // fall edge
-        flag_f_r = 1;
-    } else if (events == 0x8) { // rise edge
+bool timer_callback(repeating_timer_t *rt)
+{
+    timer_estado = 1;
+    return true;
+}
+
+void btn_callback(uint gpio, uint32_t events)
+{
+    if (events == 0x4)
+    { // fall edge
+        flag_f_r = !flag_f_r;
+    }
+    else if (events == 0x8)
+    { // rise edge
     }
 }
 
-int main() {
+int main()
+{
     stdio_init_all();
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
@@ -26,10 +38,32 @@ int main() {
     gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
                                        &btn_callback);
 
-    while (true) {
+    int timer_freq_hz = 2;        // p/ 500ms de intervalo
+    repeating_timer_t timer_info; // armazena infos do timer
 
-        if (flag_f_r) {
-            flag_f_r = 0;
+    int ta_ligado = 0;
+
+    while (true)
+    {
+        if (flag_f_r)
+        {
+            // inicia o timer só se ele não estiver rodando já
+            if (!add_repeating_timer_us(1000000 / timer_freq_hz, timer_callback, NULL, &timer_info))
+            {
+                printf("Falha ao iniciar o timer\n");
+            }
+            while (flag_f_r)
+            {
+                if (timer_estado)
+                {
+                    // printf("Hello timer!!!!!!!!!!!!\n");
+                    ta_ligado = !ta_ligado;
+                    gpio_put(LED_PIN_R, !ta_ligado);
+                    timer_estado = 0;
+                }
+            }
+            cancel_repeating_timer(&timer_info);
+            gpio_put(LED_PIN_R, 0);
         }
     }
 }
