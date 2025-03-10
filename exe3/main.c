8768c65a42e0e4dc884a81356b3ec5cfb0d17525
iara.vsp <iaravsp@al.insper.edu.br>
2025-03-10 13:11:26 -0300

    exe3

--- exe3/main.c
+++ exe3/main.c
@@ -6,19 +6,32 @@ const int BTN_PIN_R = 28;
 const int LED_PIN_R = 4;
 
 volatile int flag_f_r = 0;
+volatile bool alarme_disparado = false;
 
-void btn_callback(uint gpio, uint32_t events) {
-    if (events == 0x4) { // fall edge
+int64_t alarme_callback(alarm_id_t id, void *user_data)
+{
+    alarme_disparado = true;
+    return 0;
+}
+
+void btn_callback(uint gpio, uint32_t events)
+{
+    if (events == 0x4)
+    { // fall edge
         if (gpio == BTN_PIN_R)
+        {
             flag_f_r = 1;
-
-    } else if (events == 0x8) { // rise edge
+        }
+    }
+    else if (events == 0x8)
+    { // rise edge
         if (gpio == BTN_PIN_R)
             flag_f_r = 0;
     }
 }
 
-int main() {
+int main()
+{
     stdio_init_all();
 
     gpio_init(LED_PIN_R);
@@ -31,9 +44,23 @@ int main() {
     gpio_set_irq_enabled_with_callback(
         BTN_PIN_R, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &btn_callback);
 
-    while (true) {
+    alarm_id_t alarme;
 
-        if (flag_f_r) {
+    while (true)
+    {
+
+        if (flag_f_r)
+        {
+            alarme = add_alarm_in_ms(500, alarme_callback, NULL, false);
+            if (alarme_disparado)
+            {
+                alarme_disparado = 0;
+                gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R));
+            }
+        }
+        else
+        {
+            cancel_alarm(alarme);
         }
     }
 }
