a1494038b5b43a12daa3a246625ddf3867933b56
iara.vsp <iaravsp@al.insper.edu.br>
2025-03-12 20:44:16 -0300

    exe3

--- exe3/main.c
+++ exe3/main.c
@@ -5,7 +5,8 @@
 const int BTN_PIN_R = 28;
 const int LED_PIN_R = 4;
 
-volatile int flag_f_r = 0;
+volatile int flag_apertou = 0;
+volatile int flag_soltou = 0;
 volatile bool alarme_disparado = false;
 
 int64_t alarme_callback(alarm_id_t id, void *user_data)
@@ -20,14 +21,14 @@ void btn_callback(uint gpio, uint32_t events)
     { // fall edge
         if (gpio == BTN_PIN_R)
         {
-            flag_f_r = 1;
+            flag_apertou = 1;
         }
     }
 
     else if (events == 0x8)
     { // rise edge
         if (gpio == BTN_PIN_R)
-            flag_f_r = 0;
+            flag_soltou = 1;
     }
 }
 
@@ -50,21 +51,25 @@ int main()
     while (true)
     {
 
-        if (flag_f_r)
+        if (flag_apertou)
         {
             alarme = add_alarm_in_ms(500, alarme_callback, NULL, false);
+            flag_apertou = 0;
+            printf("apertouuuu!!!!!!!!!!!!!!!!!!!!\n");
+        }
+        if (flag_soltou)
+        {
             if (!alarme)
             {
                 printf("Failed to add timer\n");
             }
             if (alarme_disparado)
             {
-                alarme_disparado = 0;
+                printf("Alarme disparou!!!!!!!!!!!!!!!!!!!!\n");
                 gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R));
+                alarme_disparado = 0;
             }
-        }
-        else
-        {
+            flag_soltou = 0;
             cancel_alarm(alarme);
         }
     }
